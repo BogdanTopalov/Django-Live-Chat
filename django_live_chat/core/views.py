546af186c78322766a31137e602c742ba4328e94
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-10-29 22:59:08 +0300

    Update views.py

--- django_live_chat/core/views.py
+++ django_live_chat/core/views.py
@@ -1,13 +1,36 @@
 from django.contrib.auth import login
+
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView
 
-from core.forms import RegisterForm
+from core.forms import RegisterForm, CreateRoomForm
+from core.models import User
+from room.models import Message, Room
 
 
 class HomeView(TemplateView):
     template_name = 'core/home_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+
+        if self.request.user.is_authenticated:
+            current_user = User.objects.get(id=self.request.user.id)
+            user_rooms = Room.objects.filter(creator_id=self.request.user.id)
+
+            context['user_rooms'] = user_rooms
+
+            if Message.objects.filter(user=current_user):
+                unread_messages = Message.objects.filter(
+                    date_added__gt=current_user.last_online
+                )
+                context['unread_messages'] = unread_messages
+                context['unread_messages_count'] = {
+                    f"{room.id}": len([msg for msg in unread_messages if msg.room.id == room.id])
+                    for room in user_rooms
+                }
+        return context
 
 
 class RegisterUserView(CreateView):
@@ -31,3 +54,23 @@ class LoginUserView(LoginView):
 
 class LogoutUserView(LogoutView):
     next_page = reverse_lazy('home page')
+
+
+class CreateRoomView(CreateView):
+    model = Room
+    template_name = 'room/create_room_page.html'
+    form_class = CreateRoomForm
+
+    def form_valid(self, form):
+        """ Set room creator. """
+        if self.request.user.is_anonymous:
+            current_user = User.objects.create_user(username='Anonymous')
+        else:
+            current_user = self.request.user
+
+        form.instance.creator = current_user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        """ Redirect to the newly-created room. """
+        return reverse_lazy('room', kwargs={'slug': self.object.slug})
