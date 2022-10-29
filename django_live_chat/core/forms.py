19c937a58b05e628a26df7917f8dd13c44c9320a
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-10-29 22:58:32 +0300

    Update forms.py

--- django_live_chat/core/forms.py
+++ django_live_chat/core/forms.py
@@ -1,5 +1,8 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
+from core.models import User
+from room.models import Room
 
 
 class RegisterForm(UserCreationForm):
@@ -13,3 +16,9 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+
+
+class CreateRoomForm(forms.ModelForm):
+    class Meta:
+        model = Room
+        fields = ['name', ]
